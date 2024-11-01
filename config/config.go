f5910ec30eac97fd0abe751a20ab45622ab9ccdb
FudS <matartevit@gmail.com>
2024-11-01 22:56:28 +0300

    added init config

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+	"github.com/FudSy/webapi/pkg/models"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+func Initialize() {
+	// initializing .env file
+	err := godotenv.Load()
+  	if err != nil {
+    	logrus.Fatal("Error loading .env file")
+  	}
+	// initializing DB
+	dsn := os.Getenv("DB")
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logrus.Fatal("Error opening postgres DB")
+	}
+	// migrating DB
+	if err = DB.AutoMigrate(&models.Users{}); err != nil {
+		logrus.Fatal("Error migrating DB")
+	}
+	
+}
